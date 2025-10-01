e55873a4a4a1c028de439b52c9b06198ee1b154a
Ylana <ylana123@gmail.ru>
2025-10-01 16:36:11 +0300

    Add TodoItem model, TodoListManager and basic display functionality

---
+++ TodoListApp1/Managers/TodoListManager.cs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListApp1.Models;
+
+namespace TodoListApp1.Managers
+{
+    internal class TodoListManager
+    {
+
+        private List<TodoItem> _todoList = new List<TodoItem>();
+
+        public TodoListManager()
+        {
+            _todoList.Add(new TodoItem(1, "Buy groceries"));
+            _todoList.Add(new TodoItem(2, "Read a book"));
+            _todoList.Add(new TodoItem(3, "Go for a walk"));
+        }
+
+        public void DisplayTodoList()
+        {
+            Console.WriteLine("\n--- Your To-Do List ---");
+            if (_todoList.Count == 0)
+            {
+                Console.WriteLine("Your list is empty!");
+            }
+            else
+            {
+                foreach (var item in _todoList)
+                {
+                    Console.WriteLine($"{item.Id}. {item.GetStatusDisplay()} {item.Description}");
+                }
+            }
+            Console.WriteLine("---------");
+        }
+    }
+}
--- TodoListApp1/Program.cs
+++ TodoListApp1/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+using TodoListApp1.Managers;
+
+namespace TodoListApp
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Welcome to the To-Do List Application!");
+            var todoManager = new TodoListManager();
+            todoManager.DisplayTodoList();
+            Console.WriteLine("\nPress any key to exit…");
+            Console.ReadKey();
+        }
+    }
+
+}
